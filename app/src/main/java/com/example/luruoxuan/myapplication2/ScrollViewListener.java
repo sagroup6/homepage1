fba02c5eacb84a3e0354ecab55730c80c0c29c28
luruoxuan <luruoxuan@luruoxuandeMacBook-Air.local>
2018-05-23 10:21:19 +0800

    Initial commit

---
+++ app/src/main/java/com/example/luruoxuan/myapplication2/ScrollViewListener.java
@@ -0,0 +1,7 @@
+package com.example.luruoxuan.myapplication2;
+
+public interface ScrollViewListener {
+
+    void onScrollChanged(ObservableScrollView scrollView, int x, int y, int oldx, int oldy);
+
+}
